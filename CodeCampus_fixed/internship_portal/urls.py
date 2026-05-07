22e693404212661a5dd46687cbf8cff13175be95
erenbige <c.eren12@hotmail.com>
2026-05-07 23:13:03 +0300

    render

--- CodeCampus_fixed/internship_portal/urls.py
+++ CodeCampus_fixed/internship_portal/urls.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')), 
+
+    path('', TemplateView.as_view(template_name='login.html')),
+
+    path(
+        'student-dashboard/',
+        TemplateView.as_view(
+            template_name='student-dashboard.html'
+        )
+    ),
+
+    path(
+        'company-dashboard/',
+        TemplateView.as_view(
+            template_name='company-dashboard.html'
+        )
+    ),
+
+    path('api/users/', include('users.urls')),
     path('api/jobs/', include('jobs.urls')),
 ]
